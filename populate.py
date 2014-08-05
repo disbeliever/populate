a499eab03f92bc40f944d3aecf2891d49b145fb4
disbeliever <nerevar101@mail.ru>
2014-08-05 22:22:01 +0400

    working on parsing args. broken

--- populate.py
+++ populate.py
@@ -1,10 +1,31 @@
-#/usr/bin/python2
+#!/usr/bin/python2
 #-*- coding: utf-8 -*-
 
 import sys
+import getopt
+
+
+def action_fill(template):
+    pass
+
+
+def parse_args():
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:], 'h')
+    except getopt.GetoptError as e:
+        print "error:", e
+        return
+
+    args = args[1:]
+    action = args[0]
+    if (action == "fill"):
+        pass
+    if (len(args) > 1):
+        print "warning: too many args for this action. Using only '{0}'".format(args[0])
 
 
 def main():
+    parse_args()
     return 0
 
 if __name__ == "__main__":
